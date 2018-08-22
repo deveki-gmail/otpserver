e51241ebe7d9fee1e67604821a73f24211f1dfd3
devekinandanmishra <devekinandan.mishra@synechron.com>
2018-08-22 15:03:08 +0530

    initial otp server code

---
+++ src/main/java/com/example/otpserver/OtpserverApplication.java
@@ -0,0 +1,12 @@
+package com.example.otpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpserverApplication.class, args);
+	}
+}
