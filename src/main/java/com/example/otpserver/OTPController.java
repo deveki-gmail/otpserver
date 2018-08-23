0e8fe4b5c99bd14910558e2f9aeddac34b6167c9
devekinandanmishra <devekinandan.mishra@synechron.com>
2018-08-23 16:36:06 +0530

    code modified

--- src/main/java/com/example/otpserver/OTPController.java
+++ src/main/java/com/example/otpserver/OTPController.java
@@ -5,16 +5,19 @@ import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OTPController {
 	
 	@GetMapping("/otp/{userId}")
-	public ResponseEntity<String> genOtp(){
+	public ResponseEntity<String> genOtp(@PathVariable("userId") String userId){
 		final Random random = new Random();
 		final String otp = random.nextInt(9)+""+random.nextInt(9)+""+random.nextInt(9)+""+random.nextInt(9);
-		return new ResponseEntity<>(otp, HttpStatus.OK);
+		final String msg = "The OTP for user id '"+userId+"' is : "+otp;
+		System.out.println(msg);
+		return new ResponseEntity<>(msg, HttpStatus.OK);
 	}
 	
 }
