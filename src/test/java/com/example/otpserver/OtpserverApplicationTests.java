f243ee31463fde9ad807f93d1599a742ffe3fcfa
deveki-gmail <devekinandan@gmail.com>
2018-09-12 15:25:57 +0530

    Update OtpserverApplicationTests.java

--- src/test/java/com/example/otpserver/OtpserverApplicationTests.java
+++ src/test/java/com/example/otpserver/OtpserverApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import junit.framework.Assert;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OtpserverApplicationTests {
@@ -12,5 +12,48 @@ public class OtpserverApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test 
+	public void test1() {
+		Assert.assertEquals(true, true);
+	} 
+	
+	@Test 
+	public void test2() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test 
+	public void test3() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test 
+	public void test4() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test 
+	public void test5() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test 
+	public void test6() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test 
+	public void test7() {
+		Assert.assertEquals(true, true);
+	}
 }
